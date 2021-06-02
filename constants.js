8caf4952a22947c41e09d9d51e1d3be632d2e9dc
eprofessor <eprof1@gmail.com>
2021-06-02 15:58:15 -0500

    track constant.js changes, remove from gitignore

---
+++ constants.js
@@ -0,0 +1,18 @@
+// PgP 11/17/2019 MIS362-03 BUSO
+// this file contains your first name, last name and StarID to personalize your website
+// save this file as constants.js in the course root folder
+
+var xFirstName = "Mary";
+
+var xLastName = "Paulson";
+
+var xStarID = "mp5678pm";
+
+// PgP 11/17/2019
+// Must load each file manually for each student, because they do not necessarily have VPN access to T:
+// these variables are used to identify your assignments, attendance and projects for both submission and retrieval
+// entering the wrong values, or changing values after original submission can result in unexpected assignment behavior and other problems-
+// you are advised to get this right the first time!
+
+
+// *** end of constants.js  file***
